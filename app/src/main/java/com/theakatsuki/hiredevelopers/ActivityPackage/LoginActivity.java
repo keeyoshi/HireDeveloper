38aab27b0c89d6872ad2e7bb8efec55163dd0f6b
Ninjeng <ninzenlama21@gmail.com>
2020-07-05 10:45:48 +0545

    -> Bugs fixed from RegisterActivity -> Path way build with in the system

--- app/src/main/java/com/theakatsuki/hiredevelopers/ActivityPackage/LoginActivity.java
+++ app/src/main/java/com/theakatsuki/hiredevelopers/ActivityPackage/LoginActivity.java
@@ -21,20 +21,24 @@ import com.theakatsuki.hiredevelopers.R;
 
 public class LoginActivity extends AppCompatActivity {
     EditText etusername, etpassword;
-    Button btnLogin;
+    Button btnLogin,btnCreateNewAccount;
     ProgressBar progressBar;
     FirebaseAuth firebaseAuth;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
+        firebaseAuth = FirebaseAuth.getInstance();
         setContentView(R.layout.activity_login);
+        progressBar= findViewById(R.id.progressbar);
         etusername = findViewById(R.id.etemail);
         etpassword = findViewById(R.id.etpassword);
         btnLogin = findViewById(R.id.btnLogin);
+        btnCreateNewAccount = findViewById(R.id.btncreateNewUser);
         btnLogin.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View view) {
+                progressBar.setVisibility(View.VISIBLE);
                 String email = etusername.getText().toString();
                 String password = etpassword.getText().toString();
                 firebaseAuth.signInWithEmailAndPassword(email,password)
@@ -46,18 +50,25 @@ public class LoginActivity extends AppCompatActivity {
                                     Intent intent = new Intent(getApplicationContext(),MainActivity.class);
                                     intent.addFlags(Intent.FLAG_ACTIVITY_CLEAR_TASK|Intent.FLAG_ACTIVITY_NEW_TASK);
                                     startActivity(intent);
-                                    progressBar.setVisibility(View.INVISIBLE);
+                                    progressBar.setVisibility(View.GONE);
                                     finish();
 
                                 }
                                 else {
-                                    progressBar.setVisibility(View.INVISIBLE);
+                                    progressBar.setVisibility(View.GONE);
                                     Toast.makeText(LoginActivity.this, "Authentication Failed", Toast.LENGTH_SHORT).show();
                                     Vibrator vibrator = (Vibrator)getSystemService(VIBRATOR_SERVICE);
                                     vibrator.vibrate(500);
                                 }
                             }
                         });
+            }
+        });
+        btnCreateNewAccount.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                Intent intent = new Intent(getApplicationContext(),RegisterActivity.class);
+                startActivity(intent);
             }
         });
 
--- app/src/main/java/com/theakatsuki/hiredevelopers/ActivityPackage/RegisterActivity.java
+++ app/src/main/java/com/theakatsuki/hiredevelopers/ActivityPackage/RegisterActivity.java
@@ -3,6 +3,7 @@ package com.theakatsuki.hiredevelopers.ActivityPackage;
 import androidx.annotation.NonNull;
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
 import android.os.Bundle;
 import android.view.View;
 import android.widget.Button;
@@ -19,7 +20,7 @@ import com.theakatsuki.hiredevelopers.Model.User;
 import com.theakatsuki.hiredevelopers.R;
 
 public class RegisterActivity extends AppCompatActivity {
-    EditText username, password;
+    EditText username, password,fullname,phoneNumber, email;
     Button btnRegister;
     FirebaseAuth firebaseAuth;
     ProgressBar progressBar;
@@ -28,31 +29,42 @@ public class RegisterActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_register);
-        username = findViewById(R.id.email);
+        firebaseAuth = FirebaseAuth.getInstance();
+        username = findViewById(R.id.username);
+        progressBar= findViewById(R.id.progressbar);
         password = findViewById(R.id.password);
+        email = findViewById(R.id.email);
+        fullname = findViewById(R.id.name);
+        phoneNumber = findViewById(R.id.phonenumber);
         btnRegister = findViewById(R.id.register);
 
         btnRegister.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View view) {
-                final String email = username.getText().toString();
+                progressBar.setVisibility(View.VISIBLE);
+                final String userName = username.getText().toString();
                 final String pass = password.getText().toString();
-                firebaseAuth.createUserWithEmailAndPassword(username.getText().toString(),password.getText().toString())
+                final String name = fullname.getText().toString();
+                final String emailaddress = email.getText().toString();
+                final String  number= phoneNumber.getText().toString();
+                firebaseAuth.createUserWithEmailAndPassword(emailaddress,pass)
                         .addOnCompleteListener(new OnCompleteListener<AuthResult>() {
                             @Override
                             public void onComplete(@NonNull Task<AuthResult> task) {
                                 if( task.isSuccessful())
                                 {
-                                    User user = new User(FirebaseAuth.getInstance().getCurrentUser().getUid(),email,pass);
+                                    User user = new User(FirebaseAuth.getInstance().getCurrentUser().getUid(),name,number,userName,emailaddress,pass);
                                     FirebaseDatabase.getInstance().getReference("Users")
                                             .child(FirebaseAuth.getInstance().getCurrentUser().getUid())
                                             .setValue(user);
-                                    progressBar.setVisibility(View.INVISIBLE);
+                                    progressBar.setVisibility(View.GONE);
                                     Toast.makeText(getApplicationContext(), "User created", Toast.LENGTH_SHORT).show();
+                                    Intent intent = new Intent(getApplicationContext(),LoginActivity.class);
+                                    startActivity(intent);
                                 }
                                 else
                                 {
-                                    progressBar.setVisibility(View.INVISIBLE);
+                                    progressBar.setVisibility(View.GONE);
                                     Toast.makeText(getApplicationContext(), task.getException().getMessage(), Toast.LENGTH_SHORT).show();
                                 }
 
