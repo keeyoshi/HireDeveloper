a29ec4b9da97e610f46124625f696ce3a8431a08
anusha890 <anushapokharel890@gmail.com>
2020-08-11 14:06:17 +0545

    commitfr8y8ur'oi

--- app/src/main/java/com/theakatsuki/hiredevelopers/Activity/SplashActivity.java
+++ app/src/main/java/com/theakatsuki/hiredevelopers/Activity/SplashActivity.java
@@ -6,7 +6,6 @@ import android.content.Intent;
 import android.os.Bundle;
 import android.os.Handler;
 import android.util.Log;
-
 import com.google.firebase.auth.FirebaseAuth;
 import com.google.firebase.auth.FirebaseUser;
 import com.theakatsuki.hiredevelopers.R;
@@ -16,6 +15,7 @@ public class SplashActivity extends AppCompatActivity {
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
+
         setContentView(R.layout.activity_splash);
         Handler handler = new Handler();
         handler.postDelayed(new Runnable() {
@@ -30,4 +30,6 @@ public class SplashActivity extends AppCompatActivity {
     }
 
 }
+
+
 
