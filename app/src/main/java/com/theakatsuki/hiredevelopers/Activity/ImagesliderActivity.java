0fe8772e9ac3feb73f88c5a03c98101060d8a9a7
anusha890 <anushapokharel890@gmail.com>
2020-08-11 13:25:16 +0545

    commit atjfhgku

--- app/src/main/java/com/theakatsuki/hiredevelopers/Activity/ImagesliderActivity.java
+++ app/src/main/java/com/theakatsuki/hiredevelopers/Activity/ImagesliderActivity.java
@@ -2,7 +2,10 @@ package com.theakatsuki.hiredevelopers.Activity;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
 import android.widget.ImageView;
 import android.widget.ViewFlipper;
 
@@ -10,10 +13,21 @@ import com.theakatsuki.hiredevelopers.R;
 
 public class ImagesliderActivity extends AppCompatActivity {
 ViewFlipper viewFlipper;
+Button button;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_imageslider_acitivity);
+        button=findViewById(R.id.btnjoin);
+
+        button.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                    Intent intent = new Intent(ImagesliderActivity.this, LoginActivity.class);
+                    startActivity(intent);
+
+            }
+        });
 
        int imgarray[]= {R.drawable.one,R.drawable.two,R.drawable.three,R.drawable.four};
         viewFlipper= findViewById(R.id.viewflipper);
