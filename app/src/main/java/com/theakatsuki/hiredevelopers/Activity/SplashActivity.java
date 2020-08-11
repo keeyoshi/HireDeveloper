05ce488e7aa9eafc8f42ca7bc5870a9d3dadb97a
Keeyoshi <keeyoshi99@gmail.com>
2020-08-11 20:22:48 +0545

    Resolved Issued

--- app/src/main/java/com/theakatsuki/hiredevelopers/Activity/SplashActivity.java
+++ app/src/main/java/com/theakatsuki/hiredevelopers/Activity/SplashActivity.java
@@ -4,8 +4,8 @@ import androidx.appcompat.app.AppCompatActivity;
 
 import android.content.Intent;
 import android.os.Bundle;
+import android.os.Handler;
 import android.util.Log;
-
 import com.google.firebase.auth.FirebaseAuth;
 import com.google.firebase.auth.FirebaseUser;
 import com.theakatsuki.hiredevelopers.R;
@@ -15,10 +15,21 @@ public class SplashActivity extends AppCompatActivity {
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_splash);
 
+        setContentView(R.layout.activity_splash);
+        Handler handler = new Handler();
+        handler.postDelayed(new Runnable() {
+            @Override
+            public void run() {
+                Intent intent = new Intent(SplashActivity.this, ImagesliderActivity.class);
+                startActivity(intent);
+                finish();
+            }
+        },1000);
 
     }
 
 }
+
+
 
